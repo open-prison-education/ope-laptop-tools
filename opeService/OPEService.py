1d0b3044eb7e06a14fe93c64f624ee9e3d79e983
Mustafa Alshehab <mustafa.alshehab@hotmail.com>
2025-08-07 16:06:39 -0700

    treat opeService as a module

--- opeService/OPEService.py
+++ opeService/OPEService.py
@@ -24,15 +24,6 @@ import subprocess
 import sys
 import os
 import random
-from pathlib import Path
-
-# Get the absolute path to the project root directory
-project_root = Path(__file__).resolve().parent.parent
-print(f"project_root: {project_root}")
-
-# Add project root to Python path if not already there
-if str(project_root) not in sys.path:
-    sys.path.insert(0, str(project_root))
 
 from collections import OrderedDict
 
